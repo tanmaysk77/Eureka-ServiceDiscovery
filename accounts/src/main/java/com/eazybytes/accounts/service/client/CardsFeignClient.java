be5191505c5c2c693b1602d2ff669cf5eaf84694
tanmaysk77 <tanmaykunjir@gmail.com>
2024-08-18 13:27:20 +0530

    "Code Commit"

---
+++ accounts/src/main/java/com/eazybytes/accounts/service/client/CardsFeignClient.java
@@ -0,0 +1,15 @@
+package com.eazybytes.accounts.service.client;
+
+import com.eazybytes.accounts.dtos.CardsDto;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@FeignClient("cards")           // This should mach with name used by service to get registered inside eureka server
+public interface CardsFeignClient {
+
+    @GetMapping(value = "/api/fetch", consumes = "application/json")
+    public ResponseEntity<CardsDto> fetchCardDetails(@RequestParam String mobileNumber);    // This method name can change but paraeters should match
+}
+
---
+++ accounts/src/main/java/com/eazybytes/accounts/service/client/LoansFeignClient.java
@@ -0,0 +1,14 @@
+package com.eazybytes.accounts.service.client;
+
+import com.eazybytes.accounts.dtos.LoansDto;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@FeignClient("loans")           // // This should mach with name used by service to get registered inside eureka server
+public interface LoansFeignClient {
+
+    @GetMapping(value = "/api/fetch", consumes = "application/json")
+    public ResponseEntity<LoansDto> fetchLoanDetails(@RequestParam String mobileNumber);    // This method name can change but request arguements and return type should match with original request
+}
